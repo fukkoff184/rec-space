ba0233e655414cfe315d96a9337f0f4ee9ca2374
paulotr <paulotrindadecunha@gmail.com>
2023-02-03 12:17:35 -0300

    project recon-space update00

--- setup/settings.py
+++ setup/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-l1!j3bx-y#issx)zk+u1(j$c-5r3sz#c!*$un!cfi0-_a)6^tw'
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
